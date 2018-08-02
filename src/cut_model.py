92bb835a473fd878ad120c2c564ad284d506aedc
Lorenzo <lorenzo.m.romanelli@gmail.com>
2018-08-02 20:27:48 +0200

    Add script for making smaller datasets

---
+++ src/cut_model.py
@@ -0,0 +1,14 @@
+import environment
+from gensim.models import KeyedVectors
+
+def cut(modelpath, outpath, nitems):
+    model = KeyedVectors.load_word2vec_format(modelpath, binary=True, limit=nitems)
+    model.save_word2vec_format(fname=outpath, binary=True)
+
+if __name__ == "__main__":
+    modelsdir = environment.MODELS_DIR
+    modelname = "GoogleNews_full.bin.gz"
+    out = "GoogleNews.bin.gz"
+    limit = 500000
+    cut(modelsdir + modelname, modelsdir + out, limit)
+    print("Model cut.")
