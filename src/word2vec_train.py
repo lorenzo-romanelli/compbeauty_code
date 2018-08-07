4c943d22ffb08be43ec21c5b31fc45326a6339b8
Lorenzo Romanelli <lorenzo.m.romanelli@gmal.com>
2018-08-07 21:24:15 +0200

    Modify training script

--- src/word2vec_train.py
+++ src/word2vec_train.py
@@ -68,21 +68,23 @@ if __name__ == "__main__":
     print(path)
     
     # Train simple W2V model
-    modelname = "word2vec_sg.model"
-    if not os.path.isfile(modelsdir + modelname):
-        corpus = SQLiteCorpus(databasepath)  # a memory-friendly iterator
-        model = Word2Vec(corpus, sg=1, workers=4)
-        model.save(modelsdir + modelname)
-        print("W2V model trained.")
-    else:
-        print("W2V model already exists!")
+    #modelname = "word2vec_sg.model"
+    #corpus = SQLiteCorpus(databasepath)  # a memory-friendly iterator
+    #model = Word2Vec(corpus, sg=1, workers=4)
+    #model.save(modelsdir + modelname)
+    #if not os.path.isfile(modelsdir + modelname):
+    #    print("W2V model trained.")
+    #else:
+    #    print("W2V model already exists! Overwriting...")
 
     # Train W2V model using bigrams
     modelname = "word2vec_sg_bigrams.model"
+    corpus = SQLiteCorpus(databasepath, bigrams=True)
+    model = Word2Vec(corpus, sg=1, workers=4, size=200)
+    model.wv.save_word2vec_format(modelsdir+modelname, binary=True)
+    #model.save(modelsdir + modelname)
+    
     if not os.path.isfile(modelsdir + modelname):
-        corpus = SQLiteCorpus(databasepath, bigrams=True)
-        model = Word2Vec(corpus, sg=1, workers=4)
-        model.save(modelsdir + modelname)
         print("W2V bigrams model trained.")
     else:
         print("W2V bigrams model already exists!")
